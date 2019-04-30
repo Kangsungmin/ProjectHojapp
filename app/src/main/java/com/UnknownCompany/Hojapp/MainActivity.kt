cbff0b94f8cd40e99c76dd77f4e9ea7a92aa9952
smkang <belo7@naver.com>
2019-04-30 19:23:42 +0900

    [Issue]              로그인 UI 구성              [Result]              로그인 UI 구성 개발중

--- app/src/main/java/com/UnknownCompany/Hojapp/MainActivity.kt
+++ app/src/main/java/com/UnknownCompany/Hojapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.UnknownCompany.Hojapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var joinActivityButton = findViewById<View>(R.id.joinButton) as Button
+
+        joinActivityButton.setOnClickListener{
+            val nextIntent = Intent(this, JoinActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
