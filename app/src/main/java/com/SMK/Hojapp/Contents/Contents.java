f959fbe32539ed1d292b3ee2989d02b760738e4d
smkang <belo7@naver.com>
2019-06-12 15:52:53 +0900

    [Issue] 뉴스피드 개발 [Result] 뉴스피드 Write페이지 UI업데이트

--- app/src/main/java/com/SMK/Hojapp/Contents/Contents.java
+++ app/src/main/java/com/SMK/Hojapp/Contents/Contents.java
@@ -3,13 +3,15 @@ package com.SMK.Hojapp.Contents;
 public class Contents {
     public String title;
     public String body;
+    public int updateTime;
 
     public Contents() {
 
     }
 
-    public Contents(String title, String body) {
+    public Contents(String title, String body, int updateTime) {
         this.title = title;
         this.body = body;
+        this.updateTime = updateTime;
     }
 }
--- app/src/main/java/com/SMK/Hojapp/Contents/WriteContentsActivity.java
+++ app/src/main/java/com/SMK/Hojapp/Contents/WriteContentsActivity.java
@@ -1,28 +1,34 @@
 package com.SMK.Hojapp.Contents;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import com.SMK.Hojapp.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import org.w3c.dom.Text;
 
 public class WriteContentsActivity extends AppCompatActivity implements View.OnClickListener {
     private DatabaseReference mDatabase;
-
+    private TextInputEditText contentsTitleInput;
+    private TextInputEditText contentsBodyInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_contents);
 
         findViewById(R.id.completeButton).setOnClickListener(this);
+        contentsTitleInput = (TextInputEditText) findViewById(R.id.contentsTitle);
+        contentsBodyInput = (TextInputEditText) findViewById(R.id.contentsBody);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
     public void onClick(View view) {
-        writeNewContents("testID", "inputTitle", "inputBody");
+        //writeNewContents("testID", "inputTitle", "inputBody");
+        //카테고리, 작성자 정보, 제목, 내용, 시간 등 업로드
     }
 
     private void writeNewContents(String userId, String title, String body) {
