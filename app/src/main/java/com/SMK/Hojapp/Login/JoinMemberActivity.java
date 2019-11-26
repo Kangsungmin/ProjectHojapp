09e367e4906ac2f54d74cfd63d5936230c2fce83
Kangsungmin <belo7@naver.com>
2019-11-26 21:32:15 +0900

    [Issue] 회원가입 및 로그인 [Result] 회원가입 시 구글계정 등록 여부 확인 쿼리에 대한 콜백이벤트 구현

--- app/src/main/java/com/SMK/Hojapp/Login/JoinMemberActivity.java
+++ app/src/main/java/com/SMK/Hojapp/Login/JoinMemberActivity.java
@@ -1,23 +1,32 @@
 package com.SMK.Hojapp.Login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 import com.SMK.Hojapp.R;
+import com.google.firebase.database.*;
 
 
-public class JoinMemberActivity extends AppCompatActivity {
+public class JoinMemberActivity extends AppCompatActivity{
     private final String TAG = "JoinMemberActivity";
+    Context context;
     EditText m_etId;
     String m_sId;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_member);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        context = getApplicationContext();
 
-        m_etId = (EditText) findViewById(R.id.nickNameEditText);
+        m_etId = findViewById(R.id.nickNameEditText);
     }
 
 
@@ -25,13 +34,31 @@ public class JoinMemberActivity extends AppCompatActivity {
     {
         m_sId = m_etId.getText().toString();
         // 닉네임 유효성 검사
+        createNickname(m_sId);
+
     }
 
-    boolean isValidNickName(String nick)
+    void createNickname(String nick)
     {
         // 닉네임 중복, 특수문자 유효성 결과 리턴
+        mDatabase.child("accounts").child(nick).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    // 이미 존재하는 닉네임
+                    Toast.makeText(context, "이미 존재하는 닉네임입니다.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // 계정 생성 진행
+                    Toast.makeText(context, "계정생성 진행", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
 
-
-        return true;
     }
+
 }
